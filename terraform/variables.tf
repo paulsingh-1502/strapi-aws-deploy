d63d4d30f4742f1a20825cae6469a13705ad2c48
paulsingh-1502 <paulsingharon@gmail.com>
2025-10-05 17:08:38 +0530

    Initial commit - Strapi AWS deploy

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,15 @@
 variable "region" {
   default = "ap-south-1"
 }
+
+variable "db_username" {
+  description = "Username for the PostgreSQL database"
+  type        = string
+}
+
+variable "db_password" {
+  description = "Password for the PostgreSQL database"
+  type        = string
+  sensitive   = true
+}
+
