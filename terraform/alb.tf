6db576520edf0fdc7e4c76f1bbd6a08944ab8090
paulsingh-1502 <paulsingharon@gmail.com>
2025-10-05 19:43:13 +0530

    Initial commit - Strapi AWS deploy

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -1,6 +1,6 @@
 # ALB
 resource "aws_lb" "alb" {
-  name               = "${var.app_name}-alb"
+  name               = "${var.app_name}-alb-v2"
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.alb_sg.id]
@@ -9,10 +9,10 @@ resource "aws_lb" "alb" {
 
 # Target Group
 resource "aws_lb_target_group" "strapi" {
-  name     = "${var.app_name}-tg"
-  port     = var.container_port
-  protocol = "HTTP"
-  vpc_id   = aws_vpc.main.id
+  name        = "${var.app_name}-tg-v2"
+  port        = var.container_port
+  protocol    = "HTTP"
+  vpc_id      = aws_vpc.main.id
   target_type = "ip"
 }
 
--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -1,3 +1,3 @@
 resource "aws_ecr_repository" "strapi" {
-  name = var.app_name
+  name = "${var.app_name}-ecr-v2"
 }
